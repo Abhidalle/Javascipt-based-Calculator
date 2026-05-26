9a90aec4103df7bde21ba449701ea5ba1e4ef270
Abhidalle <abhiadhikari2009@gmail.com>
2026-05-26 11:41:42 +0545

    Added a new feature where you can input using your keyboard

--- script.js
+++ script.js
@@ -16,3 +16,31 @@ function calculate(){
         display.value = "Error";
     }
 }
+
+
+
+// Now moving from the basic calculator to a little better one where you can type numbers from the laptop as well
+
+document.addEventListener("keydown", function(event){
+    const key = event.key;
+
+    const allowedKeys = ["0","1","2","3","4","5","6","7","8","9","+","-","*","/", "."];
+     
+    //Now to see if the user did actually put a allowed number
+    if(allowedKeys.includes(key)){
+        appendToDisplay(key);
+    }
+
+    else if(key === "Enter" || key === "="){
+        calculate();
+    }
+    else if(key === "Escape" || key === "c" || key === "C"){
+        clearDisplay();
+    }
+    else if(key === "Backspace"){
+        display.value = display.value.slice(0,-1)
+    }
+});
+
+//Now after this is done we should be able to use our own keyboard to type the numbers in the screen as well
+
